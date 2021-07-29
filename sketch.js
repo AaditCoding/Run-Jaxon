2020711e8254cd8beb4bfdbb394f144b5137bd79
AaditCoding <87564032+AaditCoding@users.noreply.github.com>
2021-07-29 16:19:50 +0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,47 @@
+var runner,runnerImg,track,trackImg,left_boundary,right_boundary;
+function preload(){
+  //pre-load images
+  runnerImg=loadAnimation("Runner-1.png","Runner-2.png");
+  trackImg=loadAnimation("path.png")
+}
+
+function setup(){
+  createCanvas(400,400);
+  //create sprites here
+  track=createSprite(200,200)
+  track.addImage(trackImg)
+  track.velocityY=4
+  track.scale=1.2
+
+  //making runner
+  runner=createSprite(180,340,50,170);
+  runner.addAnimation("running",runnerImg)
+
+  //making invisible boundary
+  left_boundary=createSprite(0,300,100,600)
+  right_boundary=createSprite(390,300,80,600)
+  left_boundary.visible=false
+  right_boundary.visible=false
+
+
+}
+
+function draw() {
+  background(0);
+  runner.x=World.mouseX
+
+  runner.collide(left_boundary)
+  runner.collide(right_boundary)
+
+  edges=createEdgeSprites();
+  runner.collide(edges[3])
+
+  if(track.y > 400){
+    track.y = height/2
+  }
+  
+  drawSprites()
+
+}
+
+
